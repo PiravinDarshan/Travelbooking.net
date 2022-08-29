c41ff1bf1f2257a75928e4f222526a53bfa5be39
JAIKANTH <JAIKANTH@JAIKANTH07>
2022-08-29 09:05:34 +0530

    Add project files.

---
+++ Travelbooking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Travelbooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string packages = File.ReadAllText("C:\\Travel Agency Offers.tsv");
+                Console.WriteLine(packages);         
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The message could not be read");
+                Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
